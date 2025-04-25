dc9e1d9e178ae29ef56668937d5f8582a1c8fbe9
Anh Huynh <66988477+vampireplus123@users.noreply.github.com>
2025-04-25 14:06:42 +0700

    Adding Castle Choosing

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
     string player_name;
     int player_choice;
+    int castle;
     cout<<" Trainer! Before you embark on your journey,\nthe Pokémon world needs to know who you are!";
    
     cout<<"Time to step up and tell us your name.\nAnd remember, a great name is the start of every great adventure!"<<endl;
@@ -35,6 +36,32 @@ int main() {
         break;
     default:
         cout<<"Professor Oak: Hmm, that doesn't seem right. Let me choose for you… Just kidding! Let's go with Pikachu, the surprise guest!";
+        break;
+    }
+    cout << "Welcome, brave adventurer!\n";
+    cout << "You find yourself standing in front of a massive, ancient castle.\n";
+    cout << "The walls are cracked with age,\n";
+    cout << "and the doors creak as they slowly open in front of you.\n";
+    cout << "Inside, there are many rooms, each more mysterious than the last.\n";
+    cout << "Some may hold treasures beyond imagination,\n";
+    cout << "while others may hide dangers lurking in the shadows.\n";
+
+    cout<<"Enter the castle, choose your room by entering a number (1, 2, or 3): ";
+    cin>>castle;
+
+    switch (castle)
+    {
+    case 1:
+        cout<<"A room full of gold coins! You can retire now, you lucky soul!";
+        break;
+    case 2:
+        cout<<"A library of ancient books! You gain immense knowledge, but beware… some knowledge comes with a price.";
+        break;
+    case 3:
+        cout<<"A sleeping dragon! You barely escape with your life. Perhaps next time, choose more wisely...";
+        break;
+    default:
+        cout<<"A secret passage to the dungeon! Beware, danger lies ahead in the dark corridors.";
         break;
     }
     return 0;
