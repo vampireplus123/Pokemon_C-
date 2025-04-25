07bbf14dd0fd28cd9269564d1281f45c1bd6995a
Anh Huynh <66988477+vampireplus123@users.noreply.github.com>
2025-04-25 12:45:39 +0700

    Update first feature

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <string.h>
+using namespace std;
 
 int main() {
+    string player_name;
+    
+    cout << "Enter your name: ";
+    cin >> player_name;
+
+    cout << "Great Start " << player_name << ", looks like you have understood the main() function properly now!" << endl;
 
     return 0;
 }
