91e9c7b4e274b0b135a555f4ac1313fd1b36cabb
Anh Huynh <66988477+vampireplus123@users.noreply.github.com>
2025-04-28 16:12:01 +0700

    Update_beside game project branch

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 
-int main() {
+using namespace std;
 
+
+void castSpell(int magicLevel)
+{
+    cout<<"Casting spell with magic level: "<<magicLevel;
+}
+void brewElixir(int &magicLevel)
+{
+    magicLevel += 10;
+}
+
+void brewPotion(int magicLevel)
+{
+    magicLevel += 50;
+}
+int main() {
+    int magicLevel = 30;
+    castSpell(magicLevel);
+
+    // Brew the Elixir of Wisdom and call castSpell to show effect
+    brewElixir(magicLevel);
+    castSpell(magicLevel);
+
+    // Brew the Potion of Swiftness and call castSpell to show effect
+    brewPotion(magicLevel);
+    
+    // Final magic level after all potions
+    cout << "Final magic level: " << magicLevel << endl;
     return 0;
 }
